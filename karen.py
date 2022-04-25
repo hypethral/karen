0231dc7a29d719a527e9297bd4855713797b8f23
hypethral <hypethral1@gmail.com>
2022-04-25 16:39:06 +0500

    Given her voice

---
+++ karen.py
@@ -0,0 +1,30 @@
+import pyttsx3
+import subprocess
+
+
+#from decouple import config
+
+USERNAME = 'dark'
+BOTNAME = 'Karen'
+
+
+engine = pyttsx3.init('espeak')
+
+# Set Rate
+engine.setProperty('rate', 140)
+
+# Set Volume
+engine.setProperty('volume', 1.0)
+
+# Set Voice (Female)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', 'en+f2')
+
+def speak(text):
+    """Used to speak whatever text is passed to it"""
+    
+    engine.say(text)
+    engine.runAndWait()
+
+
+speak("stupid raima stupid raima stupid raima stupid raima stupid raima stupid raima stupid raima stupid raima stupid raima stupid raima stupid raima")
